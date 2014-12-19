c6e2a69dcf96480afd8cc0b4b6e405d37f95ce19
Parham Alvani <parham.alvani@gmail.com>
2014-12-19 23:04:43 +0330

    Single line comment is added.

--- src/fs.h
+++ src/fs.h
@@ -4,7 +4,7 @@
 // 
 // * Creation Date : 19-12-2014
 //
-// * Last Modified : Fri 19 Dec 2014 08:21:20 PM IRST
+// * Last Modified : Fri 19 Dec 2014 11:04:01 PM IRST
 //
 // * Created By : Parham Alvani (parham.alvani@gmail.com)
 // =======================================
@@ -13,7 +13,9 @@
 
 #include "FAT.h"
 
+// Find path in filesystem tree.
 fat_dir_layout_t* find(const char* path);
-fat_dir_layout_t* parse_dir(fat_dir_layout_t dir);
+// Parse directory and set dir_size to entry number and set entry it self in fat_dir_layout*.
+fat_dir_layout_t* parse_dir(fat_dir_layout_t dir, int* dir_size);
 
 #endif
