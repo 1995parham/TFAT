ffbc78dbe65d4e122380ab404e1e03523c99a8bd
Parham Alvani <parham.alvani@gmail.com>
2014-12-21 02:19:55 +0330

    common.h & common.c checked.

--- src/common.c
+++ src/common.c
@@ -1,11 +1,11 @@
-/* 
+/*
  * In The Name Of God
  * ========================================
  * [] File Name : common.c
  *
  * [] Creation Date : 21-12-2014
  *
- * [] Last Modified : Sun 21 Dec 2014 12:56:20 AM IRST
+ * [] Last Modified : Sun 21 Dec 2014 02:19:49 AM IRST
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
@@ -18,17 +18,21 @@
 
 #include "common.h"
 
-char* rtrim(char *string){
+char *rtrim(char *string)
+{
 	char *original = string + strlen(string);
-	while(*(--original) == ' ');
+
+	while (*(--original) == ' ')
+		;
 	*(original + 1) = 0;
 	return string;
 }
 
-void die(const char *fmt, ...){
+void die(const char *fmt, ...)
+{
 	char buf[MAX_BUFF];
-	
-	va_list args; 
+	va_list args;
+
 	va_start(args, fmt);
 	vsprintf(buf, fmt, args);
 	va_end(args);
--- src/common.h
+++ src/common.h
@@ -1,11 +1,11 @@
-/* 
+/*
  * In The Name Of God
  * ========================================
  * [] File Name : common.h
  *
  * [] Creation Date : 21-12-2014
  *
- * [] Last Modified : Sun 21 Dec 2014 12:56:25 AM IRST
+ * [] Last Modified : Sun 21 Dec 2014 02:16:42 AM IRST
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
@@ -13,9 +13,9 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#define MAX_BUFF 1000 * 1000
+#define MAX_BUFF (1000 * 1000)
 
-char* rtrim(char *string);
+char *rtrim(char *string);
 void die(const char *fmt, ...);
 
 #endif
